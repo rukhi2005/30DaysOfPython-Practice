b53968cb78216b849b26a7d76ba3acffceb9ac91
Rukhaiya Mohammed <rukhaiya.mohammed.2005@gmail.com>
2026-01-16 21:33:08 +0530

    Initial commit: 30DaysOfPython practice

---
+++ Day01/helloworld.py
@@ -0,0 +1,88 @@
+#EXERCISE 1
+
+
+# x = int(input(""))
+# y = int(input(""))
+
+# add = x + y 
+# print(add)
+
+# sub = x - y
+# print(sub)
+
+# mul = x * y 
+# print(mul)
+
+# mod = x % y
+# print(mod)
+
+# div = x / y
+# print(div)
+
+# exp = x ** y
+# print(exp)
+
+# fdiv = x // y
+# print(fdiv)
+
+
+
+
+
+#EXERCISE 2
+
+
+
+# print(type(10))
+# print(type(9.8))
+# print(type(3.14))
+# print(type(4-4j))
+
+# list = ['Rukhaiya', 'Python', 'Canada']
+# print(type(list))
+
+# name = input("Enter your name : ")
+# print(type(name))
+
+# family_name = input("Enter your family name : ")
+# print(type(family_name))
+
+# country = input("Enter your country : ")
+# print(type(country))
+
+
+
+
+
+#EXERCISE 3
+
+# a = 10                          # int
+# b = 3.14                        # float
+# c = 2 + 3j                       # complex
+# name = "Rukhaiya"                # string
+# is_ok = True                    # boolean
+# my_list = [1, 2, 3]             # list
+# my_tuple = (4, 5, 6)             # tuple
+# my_set = {1, 2, 3}                # set
+# my_dict = {"a": 1, "b": 2}        # dictionary
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(name))
+# print(type(is_ok))
+# print(type(my_list))
+# print(type(my_tuple))
+# print(type(my_set))
+# print(type(my_dict))
+
+
+
+#euclidean distance
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
+
+print("Euclidean distance =", distance)
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# 30 Days of Python – My Practice
+
+This repository contains my daily practice and solutions while following the 30DaysOfPython GitHub challenge.
+
+## Goal
+- Build Python fundamentals
+- Improve problem-solving
+- Be consistent
+
+## Progress
+- Day 1 - [✅]
+...
