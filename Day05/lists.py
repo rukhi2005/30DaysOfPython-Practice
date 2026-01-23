daf6eb7191fafa4d16934608f389665bb2a3da34
Rukhaiya Mohammed <rukhaiya.mohammed.2005@gmail.com>
2026-01-23 23:47:54 +0530

    Day 6

--- Day05/lists.py
+++ Day05/lists.py
@@ -4,3 +4,38 @@
 list = []
 
 #2.
+names = ['Mike', 'Will', 'Lucas', 'Dustin', 'Eleven', 'Steve', 'Nancy', 'Jonathan', 'Max']
+
+#3.
+print(len(names))
+
+#4.
+print(names[0])
+print(names[4])
+print(names[-1])
+
+#5.
+mixed_data_types = ['Rukhaiya', 20, 152, 'Unmarried', 'Toronto, Canada']
+
+#6.
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+#7.
+print(it_companies)
+
+#8.
+print("No: of companies: ", len(it_companies))
+
+#9.
+print(it_companies[0])
+print(it_companies[2])
+print(it_companies[-1])
+
+#10.
+it_companies[2] = 'Visa'
+
+#11.
+it_companies.append('Stripe')
+
+#12.
+
